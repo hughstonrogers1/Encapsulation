889610367a3e0d176b357eb197ee141c7bef9656
hughstonrogers1 <hughstonrogers1>
2021-03-22 21:29:41 -0400

    Encapsulation

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount a = new BankAccount();
+
+            Console.WriteLine("Enter an amount to deposit into your account.");
+            double depositAmount = double.Parse(Console.ReadLine());
+
+            a.Deposit(depositAmount);
+            double userBalance = a.GetBalance();
+
+            Console.WriteLine($"Your currant account balance is: {userBalance, 0:c}");
+        }
+    }
+}
